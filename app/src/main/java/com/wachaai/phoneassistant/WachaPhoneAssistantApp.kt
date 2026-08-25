1cde3f423a2cbd7133ddfdcb919ff5f9508b4378
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-25 18:11:19 +0300

    Initialize assistant settings and report scheduler

--- app/src/main/java/com/wachaai/phoneassistant/WachaPhoneAssistantApp.kt
+++ app/src/main/java/com/wachaai/phoneassistant/WachaPhoneAssistantApp.kt
@@ -1,9 +1,12 @@
 package com.wachaai.phoneassistant
 
 import android.app.Application
+import com.wachaai.phoneassistant.data.AutoReplyRegistry
 import com.wachaai.phoneassistant.data.EncryptedFileStore
 import com.wachaai.phoneassistant.data.MessageRepository
 import com.wachaai.phoneassistant.data.SecretStore
+import com.wachaai.phoneassistant.data.SettingsStore
+import com.wachaai.phoneassistant.report.DailyReportWorker
 
 class WachaPhoneAssistantApp : Application() {
     lateinit var messageRepository: MessageRepository
@@ -12,10 +15,19 @@ class WachaPhoneAssistantApp : Application() {
     lateinit var secretStore: SecretStore
         private set
 
+    lateinit var settingsStore: SettingsStore
+        private set
+
+    lateinit var autoReplyRegistry: AutoReplyRegistry
+        private set
+
     override fun onCreate() {
         super.onCreate()
         val encryptedStore = EncryptedFileStore(this)
         messageRepository = MessageRepository(encryptedStore)
         secretStore = SecretStore(encryptedStore)
+        settingsStore = SettingsStore(encryptedStore)
+        autoReplyRegistry = AutoReplyRegistry(encryptedStore)
+        DailyReportWorker.schedule(this, settingsStore.settings.value.reportHour)
     }
 }
