638c0af0aaf2b488ede6ca34d78f117b686259f6
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-25 09:47:23 +0300

    Use stable Android 16 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.wachaai.phoneassistant"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.wachaai.phoneassistant"
